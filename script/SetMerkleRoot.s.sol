aa204e8bfb44e194e09a1b208b3cec2693a2fbd4
anotherdev <joseluismanco37@gmail.com>
2025-08-18 11:27:48 -0500

    feat: new test airdrop

--- script/SetMerkleRoot.s.sol
+++ script/SetMerkleRoot.s.sol
@@ -6,15 +6,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SetMerkleRoot is Script {
     bytes32 private _merkleRoot =
-        0x3546c721dfc9bf02a2f10b38c64b7ba6c6d3217afaba535e3ecbb59d0e0c8612;
+        0xd261cff9d5adec714268bdca43132920c4dc3c96ddd0861c81d07ba47417ebcc;
     // uint256 private _amount = 2500000 * 4 * 1e18;
     // Expiration time: 30 days from now (in seconds)
-    uint256 private _expirationTime = block.timestamp + 30 days;
+    uint256 private _expirationTime = block.timestamp + 7 days;
 
     function run() public returns (ProsperityAidrop) {
         vm.startBroadcast();
         ProsperityAidrop airdrop = ProsperityAidrop(
-            0xcc86F7903f52EEb20c512D26C829e1545D577c47
+            0x2a4871972Ece4e6E04a07E0F3e3D8168a261Dce2
         );
         ERC20 CELO = ERC20(0x471EcE3750Da237f93B8E339c536989b8978a438);
         airdrop.setMerkleRoot(
